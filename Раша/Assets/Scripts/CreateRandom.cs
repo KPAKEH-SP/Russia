51d5aed4b8429f8ba860348e81a1494e340941ee
KPAKEH-SP <54186581+KPAKEH-SP@users.noreply.github.com>
2021-02-02 17:12:49 +0400

    Рандом спавна

---
+++ Раша/Assets/Scripts/CreateRandom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandom : MonoBehaviour
+{
+    int choose;
+    public bool boy;
+    public bool girl;
+    public GameObject cam;
+
+    void Start()
+    {
+        choose = Random.Range(0, 2);
+
+        if (choose == 0)
+        {
+            boy = true;
+            cam.transform.Translate(34, 0, 0);
+        }
+
+        else if (choose == 1)
+        {
+            girl = true;
+            cam.transform.Translate(0, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
